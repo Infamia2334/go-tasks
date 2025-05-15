0d5c931386bff8a3ac5851f4dbb586c58fcc45a7
Dipan <dipanisboss@gmail.com>
2025-05-16 05:08:47 +0530

    Introduced pagination and filtering API

--- internal/delivery/http/routes/route.go
+++ internal/delivery/http/routes/route.go
@@ -6,6 +6,7 @@ import (
 )
 
 func RegisterRoutes(router *mux.Router, taskHandler *handlers.TaskHandler) {
-	router.HandleFunc("/tasks", taskHandler.GetTasks).Methods("GET")
+	router.HandleFunc("/tasks/all", taskHandler.GetTasks).Methods("GET")
 	router.HandleFunc("/task", taskHandler.CreateTask).Methods("POST")
+	router.HandleFunc("/tasks", taskHandler.GetPaginatedTasks).Methods("GET")
 }
